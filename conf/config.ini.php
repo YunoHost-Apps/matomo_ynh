be65aca6df783aa50d4460b9275b7eea03f2868f
anmol <github@datamol.org>
2018-10-30 15:26:24 +0530

    Added config.ini.php file for configuration

---
+++ conf/config.ini.php
@@ -0,0 +1,8 @@
+; <?php exit; ?> DO NOT REMOVE THIS LINE
+; file automatically generated or modified by Matomo; you can manually override the default values in global.ini.php by redefining them in this file.
+[database]
+host = "127.0.0.1"
+username = "__DB__"
+password = "__DB_PASSWORD__"
+dbname = "__DB__"
+tables_prefix = "matomo_"
