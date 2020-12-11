2931b0bf593c8df89c8e1e31eb54680bbb3443d5
ericgaspar <junk.eg@free.fr>
2020-12-11 11:39:52 +0100

    Fix

--- conf/config.ini.php
+++ conf/config.ini.php
@@ -2,7 +2,8 @@
 ; file automatically generated or modified by Matomo; you can manually override the default values in global.ini.php by redefining them in this file.
 [database]
 host = "127.0.0.1"
-username = "__DB__"
+username = "__DB_NAME__"
 password = "__DB_PASSWORD__"
-dbname = "__DB__"
+dbname = "__DB_NAME__"
 tables_prefix = "matomo_"
+
