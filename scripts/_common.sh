03442c3f6fd05821b5841f10e3d715bab93ec116
ericgaspar <junk.eg@free.fr>
2021-05-09 11:11:06 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,7 @@ YNH_PHP_VERSION="7.3"
 extra_php_dependencies="php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-mbstring"
 
 # Composer version
-composer_version=1.10.17
+composer_version=2.0.13
 
 #=================================================
 # PERSONAL HELPERS
