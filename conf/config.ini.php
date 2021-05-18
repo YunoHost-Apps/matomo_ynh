91c0a78bd414ea01ca60d9689c2d31fa04bcf8c0
ericgaspar <junk.eg@free.fr>
2021-05-18 11:40:24 +0200

    Fix

--- conf/config.ini.php
+++
@@ -1,5 +0,0 @@
-; <?php exit; ?> DO NOT REMOVE THIS LINE
-; file automatically generated or modified by Matomo; you can manually override the default values in global.ini.php by redefining them in this file.
-
-[General]
-force_ssl = 1
