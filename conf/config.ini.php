91d2d079c9c20438c971cb65bab426c76bd52a6f
ericgaspar <junk.eg@free.fr>
2021-05-18 11:38:21 +0200

    fix

--- conf/config.ini.php
+++ conf/config.ini.php
@@ -1,15 +1,5 @@
 ; <?php exit; ?> DO NOT REMOVE THIS LINE
 ; file automatically generated or modified by Matomo; you can manually override the default values in global.ini.php by redefining them in this file.
-[database]
-host = "127.0.0.1"
-username = "__DB_NAME__"
-password = "__DB_PWD__"
-dbname = "__DB_NAME__"
-tables_prefix = "matomo_"
-charset = "utf8mb4"
 
 [General]
 force_ssl = 1
-salt = "__SALT__"
-trusted_hosts[] = "__DOMAIN__"
-trusted_hosts[] = "WWW.__DOMAIN__"
