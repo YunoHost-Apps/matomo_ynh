b16d9a93bdac9522b56528b9c81292d7a474432c
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2022-11-04 21:56:12 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 YNH_PHP_VERSION=8.0
 
 # dependencies used by the app
-pkg_dependencies="php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-gd php$YNH_PHP_VERSION-cli php$YNH_PHP_VERSION-mysql php$YNH_PHP_VERSION-xml php$YNH_PHP_VERSION-mbstring"
+#REMOVEME? pkg_dependencies="php$YNH_PHP_VERSION-curl php$YNH_PHP_VERSION-gd php$YNH_PHP_VERSION-cli php$YNH_PHP_VERSION-mysql php$YNH_PHP_VERSION-xml php$YNH_PHP_VERSION-mbstring"
 
 #=================================================
 # PERSONAL HELPERS
